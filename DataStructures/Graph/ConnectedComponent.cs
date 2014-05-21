f9d0f5195f0fdd2f1283805e8e984f34aa73478e
Robert Goheen <goheen@gmail.com>
2014-05-21 22:34:06 +0100

    start on connected components

---
+++ DataStructures/Graph/ConnectedComponent.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pretero.DataStructures
+{
+    public class ConnectedComponent<T> where T: IEquatable<T>
+    {
+        private readonly Graph<T> _graph;
+
+        private Dictionary<Vertex<T>, int> _groupings = new Dictionary<Vertex<T>, int>();
+
+        public ConnectedComponent(Graph<T> graph)
+        {
+            _graph = graph;
+            ProcessGraph();
+        }
+
+        private void ProcessGraph()
+        {
+            var group = 0;
+            foreach (var vertex in _graph.Verticies)
+            {
+                
+            }
+
+        }
+    }
+}
