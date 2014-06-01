ec2e73887c033eb84102842edfa8e96278d35a88
Robert Goheen <goheen@gmail.com>
2014-06-01 08:37:50 +0100

    new base class for graph

---
+++ DataStructures/Graph/BaseGraph.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pretero.DataStructures
+{
+   public abstract class BaseGraph<T> where T : IEquatable<T>
+   {
+      protected readonly bool IsDirected;
+
+      protected BaseGraph(bool isDirected)
+      {
+         IsDirected = isDirected;
+      }
+
+      protected BaseGraph(bool isDirected, IEnumerable<T> verticies)
+      {
+         IsDirected = isDirected;
+         _verticies.AddRange(verticies.Select(x => new Vertex<T>(x)));
+      }
+
+      protected BaseGraph(bool isDirected, IEnumerable<Tuple<T, T>> items)
+      {
+         IsDirected = isDirected;
+         foreach (var item in items)
+            AddEdge(item.Item1, item.Item2);
+      }
+
+      private Vertex<T> RetrieveOrCreateVertex(T value)
+      {
+         var vertex = _verticies.SingleOrDefault(x => x.Value.Equals(value));
+
+         if (vertex == null)
+         {
+            vertex = new Vertex<T>(value);
+            _verticies.Add(vertex);
+         }
+
+         return vertex;
+      }
+
+      private readonly List<Vertex<T>> _verticies = new List<Vertex<T>>();
+
+      public IEnumerable<Vertex<T>> Verticies
+      {
+         get { return _verticies; }
+      }
+
+      public void AddEdge(T first, T second)
+      {
+         var firstVertex = RetrieveOrCreateVertex(first);
+         var secondVertex = RetrieveOrCreateVertex(second);
+
+         firstVertex.AddEdge(secondVertex);
+
+         if (!IsDirected)
+            secondVertex.AddEdge(firstVertex);
+
+         Edges++;
+      }
+
+      public IEnumerable<Vertex<T>> AdjacentTo(T first)
+      {
+         var vertex = _verticies.Single(x => x.Value.Equals(first));
+         return vertex.AdjacentVerteces;
+      }
+
+      public int Edges { get; private set; }
+   }
+}
--- DataStructures/Graph/Graph.cs
+++ DataStructures/Graph/Graph.cs
@@ -4,58 +4,14 @@ using System.Linq;
 
 namespace Pretero.DataStructures
 {
-    public class Graph<T> where T : IEquatable<T>
+   public class Graph<T> : BaseGraph<T> where T : IEquatable<T>
     {
-        public Graph() { }
+        public Graph() : base(false) { }
 
         public Graph(IEnumerable<T> verticies)
-        {
-            _verticies.AddRange(verticies.Select(x => new Vertex<T>(x)));
-        }
+           : base(false, verticies) { }
 
         public Graph(IEnumerable<Tuple<T,T>> items)
-        {
-            foreach (var item in items)
-                AddEdge(item.Item1, item.Item2);
-        }
-
-        private Vertex<T> RetrieveOrCreateVertex(T value)
-        {
-            var vertex = _verticies.SingleOrDefault(x => x.Value.Equals(value));
-
-           if (vertex == null)
-           {
-              vertex = new Vertex<T>(value);
-              _verticies.Add(vertex);
-           }
-
-           return vertex;
-        }
-
-        private readonly List<Vertex<T>> _verticies = new List<Vertex<T>>();
-
-        public IEnumerable<Vertex<T>> Verticies
-        {
-            get { return _verticies; }
-        }
-
-        public void AddEdge(T first, T second)
-        {
-            var firstVertex = RetrieveOrCreateVertex(first);
-            var secondVertex = RetrieveOrCreateVertex(second);
-
-            firstVertex.AddEdge(secondVertex);
-            secondVertex.AddEdge(firstVertex);
-
-            Edges++;
-        }
-
-        public IEnumerable<Vertex<T>> AdjacentTo(T first)
-        {
-            var vertex = _verticies.Single(x => x.Value.Equals(first));
-            return vertex.AdjacentVerteces;
-        }
-
-        public int Edges { get; private set; }
+           : base(false, items) { }
     }
 }
